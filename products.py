bf8a0430882cd544d9cd0734424437e7c233b9af
Jacob <xxbookertxx@yahoo.com.tw>
2020-09-17 16:11:21 +0800

    add header

--- products.py
+++ products.py
@@ -4,11 +4,14 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ')
-	p = []
-	p.append(name)  
-	p.append(price)
-	products.append(p)  # 7~10 可直接寫成 products.append([name, price])
+	price = int(price)
+	products.append([name, price])
 print(products)
 
 for p in products:
 	print(p[0], '的價格是', p[1])
+
+with open('products .csv', 'w', encoding='utf-8') as f:
+	f.write('商品,價格\n') 
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
